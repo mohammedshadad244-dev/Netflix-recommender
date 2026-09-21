4fd702940bec54dc1323b7b45682e579a6b3173b
Mohamed Elmekki Shadad <mohammedshadad244@gmail.com>
2026-09-21 22:20:04 +0800

    Stage 2: Add user name input and genre selection menu

--- netflix_recommender.cpp
+++ netflix_recommender.cpp
@@ -16,5 +16,31 @@ cout << "========================================" << endl;
     cout << "Press Enter to continue...";
     cin.ignore();  // Wait for user to press Enter
 
+    //Get the user name
+
+string userName;
+    cout << "Please enter your username!" << endl;
+    cin >> userName;
+    cout << "Hello " << userName << "! Let's find a movie for you" << endl << endl;
+
+    //Show genre menu
+
+int genreChoice;
+    cout << "Select a Genre:" << endl;
+    cout << "1. Action" << endl;
+    cout << "2. Romance" << endl;
+    cout << "3. Horror" << endl;
+    cout << "4. Comedy" << endl;
+    cout << "5. Documentary" << endl;
+    cout << "----------------------------------" << endl;
+    cout << "Enter your choice! (1-5)" << endl;
+    cin >> genreChoice;
+
+    // Confirm what the user picked
+    cout << endl;
+    cout << "You selected option " << genreChoice << "." << endl;
+
+
+
     return 0;
 }
